a1bd11e9bc6220cf792c99edc5ba3ca05251b05e
cosre7 <cosre7@gmail.com>
2021-09-27 17:36:53 +0900

    Ajax 활용 div refresh 적용

--- src/main/java/multiplication/multiplicationspring/controller/MultiplicationController.java
+++ src/main/java/multiplication/multiplicationspring/controller/MultiplicationController.java
@@ -2,7 +2,6 @@ package multiplication.multiplicationspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,9 +22,9 @@ public class MultiplicationController {
 		return "thymeleaf/multiplicationForm";
 	}
 	
-	@PostMapping("/")
 	@ResponseBody
-	public String create(Multiplication multiplication, Model model) {
+	@PostMapping("/")
+	public String create(Multiplication multiplication) {
 		
 		int startColumnNumber = 1; // 첫번째 곱해지는 수
 		
@@ -34,15 +33,10 @@ public class MultiplicationController {
 				multiplication.getEndRowNumber(), startColumnNumber, 
 				multiplication.getEndColumnNumber());
 		
-		model.addAttribute("multiplication", multiplication);
-		model.addAttribute("startColumnNumber", startColumnNumber);
-		model.addAttribute("table", table);
 //		Assertions.assertThat(multiplicationService).isSameAs(multiplicationService2); // test실행
-		
 		Gson gson = new Gson();
-		String gsonString = gson.toJson(table);
-		
+		String gsonString = gson.toJson(table); // 테이블 데이터를 gson을 사용하여 string 형태로 바꾼다.
 		// 리턴값에는 함수값 주지 말자!
-		return gsonString;
+		return gsonString; // string 형태의 데이터를 넘긴다.
 	}
 }
